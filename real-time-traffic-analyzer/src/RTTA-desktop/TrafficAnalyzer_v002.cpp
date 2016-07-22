8394f1beca246fc8b49bb427a4c0bc2ad3ee89f0
Besenczi Renátó <renato.besenczi@gmail.com>
2016-07-22 15:56:41 +0200

    DEBUG feature

--- real-time-traffic-analyzer/src/RTTA-desktop/TrafficAnalyzer_v002.cpp
+++ real-time-traffic-analyzer/src/RTTA-desktop/TrafficAnalyzer_v002.cpp
@@ -1,25 +1,23 @@
 /*
-* g++ TrafficAnalyzer_v002.cpp -o TrafficAnalyzer_v002 `pkg-config --cflags --libs opencv`
+* g++ TrafficAnalyzer_v002.cpp -o TrafficAnalyzer_v002 `pkg-config --cflags --libs opencv` -Wall {-DDEBUG}
 * ./TrafficAnalyzer_v002 ../test_videos/part3.mov or ./TrafficAnalyzer_v002 for webcam
 */
-
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
 
-const int white_color = 255;
 const char* cascade_file = "haarcascade_car_1.xml";
 
 int main(int argc, char** argv){
 
 	if (argc > 2){
-		std::cout << "Usage: ./TrafficAnalyzer_v002 ../test_videos/part3.mov or ./TrafficAnalyzer_v002 for webcam" << std::endl;
+		std::cout << "Usage: {./TrafficAnalyzer_v002 ../test_videos/part3.mov} or {./TrafficAnalyzer_v002} for webcam" << std::endl;
 		return 0;
 	}
 
     VideoCapture cap;
     if (argc == 2)
-        cap.open(argv[1]); //or 0 for default cam
+        cap.open(argv[1]);
     else
         cap.open(1);    //Pay attention for the device ID!!! Not good this way.
 
@@ -33,8 +31,9 @@ int main(int argc, char** argv){
 
     CascadeClassifier cascadeClassifier (cascade_file);
 
-    //For test, remove later
+#ifdef DEBUG
     VideoWriter vwr ("TestVideo.mkv", VideoWriter::fourcc('D','A','V','C'), 30, Size ( 400, 300 ), false);
+#endif
 
     for(;;){
 
@@ -54,17 +53,18 @@ int main(int argc, char** argv){
         * Use the detected_objects vector to get information, e.g.:
         * std::cout << "Number of detected objects on this image: " << detected_objects.size() << "\n";
 		*/
-
+#ifdef DEBUG
         //visualization
         for(int i = 0; i < (int)detected_objects.size(); i++)
-            rectangle(grey, detected_objects.at(i), white_color, 2);
+            rectangle(grey, detected_objects.at(i), 255, 2);
 
         imshow("Traffic Analyzer v002", grey);
 
-        //Recording
-        vwr << grey;
+        vwr << grey; //Recording
 
         if(waitKey(30) == 'c') break;
+#endif
+
     }
 
     return 0;
